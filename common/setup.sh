fc558ffdcaf3c4b67d81fae7d63afddd622f81dc
Aaron Robinson <aaron.h.robinson@gmail.com>
2021-10-13 20:20:39 -0500

    Add add_alias() function, along with my git aliases

--- common/setup.sh
+++ common/setup.sh
@@ -12,5 +12,17 @@ done < $COMMON_DIR/packagelist.txt
 for f in $COMMON_DIR/conf_writers/*.sh; do ./$f; done
 
 ## Copy Util Scripts
-mkdir ~/.util_scripts/
+mkdir -p ~/.util_scripts/
 cp -r $COMMON_DIR/util_scripts/ ~/.util_scripts/
+
+## Add Aliases if not already added
+function add_alias() {
+    if ! grep -xq "alias $1=\"$2\"" ~/.bashrc ; then
+        echo "alias $1=\"$2\"" >> ~/.bashrc
+    fi
+}
+
+add_alias gs "git status"
+add_alias gd "git diff"
+add_alias gs "git diff --cached"
+
